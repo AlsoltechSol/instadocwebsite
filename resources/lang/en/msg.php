aaa543edc3b9573d4fb9ae65cb06478d0bfd3450
Souvik <souvik.dg1193@gmail.com>
2022-12-29 22:43:22 -0500

    multilanguage

---
+++ resources/lang/en/msg.php
@@ -0,0 +1,35 @@
+<?php
+    return [
+        'About' => "About Us:",
+        'AboutDescription'=>"Our services help patients where distance is a critical factor. We bridge the gap and bring them in touch with the best medical facilities. Hence our services can help patients coming from distant places with facilities such as providing them with information for improved diagnosis and treatment. Our specific facilities will enable patients to acquire both online and offline consultations with our expert doctors, order medicine, request diagnostic tests, and get support with visa or passport-related matters, all ticketing inside India, pick up and drop services from the airport, train station and bus stop accommodation, medical guide and many more.",
+
+        'Services'=>"Our Services",
+
+        'Medical-Consultation'=>"Medical Consultation:",
+        'MedicalDescription'=>"Are you looking to consult with trustworthy doctors and get medical facilities at an affordable cost? We have the best doctors registered with us who not only provide consultation online but also can be visited offline. Call us at the below number for further details.",
+        
+        'Medical-order'=>"Medicine Order:",
+
+        'medicalOrder-description'=>"Do you want to order some medicine and wish them to be delivered in no time at a reasonable cost from outside India? We have a wonderful chain of delivery that ensures that your medicines reach you on time. Call us at the below number to order medicine.",
+
+        'labTest'=>"Lab Test:",
+
+        'labTest-description'=>"Book your lab test with our InstaDoctor registered labs. Get all your Lab tests done at a reasonable rate and get hassle-free sample collection and report delivery. Call us at the below number to book a test.",
+
+        'Visa'=>"Visa & Passports for medical visits:",
+
+        'Visa-Descreiption'=>"Our team in InstaDoctor helps you to prepare your visa application to get a quick medical visa. We also help with passport and cross-border travel for medical purposes. Call us at the below number to get assistance on your medical visits.",
+
+        'Travel'=>"Travel Support for medical purposes:",
+
+        'Travel-description'=>"We provide all sorts of travel assistance by bus, train, and flight. Don't bother about your lodging and fooding as we manage that as well during your medical visits. If required we tag along a medical attender to assist you 24*7",
+        
+        'Counter1'=>"number of patients served",
+        'Counter2'=>"number of doctors tied up",
+        'Counter3'=>"hospitals tied up",
+        'Counter4'=>"medical visits for patients",
+        
+        'Contact'=>"Contact No:",
+        'Contact-us'=>"Do you have any queries, suggestions, or requests? InstaDoctor is a chain of patient care facilities. To contact us, please fill out the form below.",
+    ]
+?>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+Route::get('/{lang}',function($lang){
+    App::setLocale($lang);
     return view('home');
 });
 
